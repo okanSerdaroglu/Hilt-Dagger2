90c35b9c8308920923a9582347e6d8d706aad9fc
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-08-23 17:58:57 +0300

    Field Injection

--- Hilt-Dagger2/app/src/main/java/com/okan/hilt_dagger2/MainActivity.kt
+++ Hilt-Dagger2/app/src/main/java/com/okan/hilt_dagger2/MainActivity.kt
@@ -2,10 +2,27 @@ package com.okan.hilt_dagger2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    // field injection
+    @Inject
+    lateinit var someClass: SomeClass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println(someClass.doSomething())
+    }
+}
+
+class SomeClass
+@Inject
+constructor() {
+    fun doSomething(): String {
+        return "Look ..."
     }
 }
